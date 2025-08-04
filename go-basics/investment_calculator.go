341206e4e60fc7e0c99d32fc5edd10e1632a5020
Anderson Almeida Rodrigues <anderson.a.rodrigues14@hotmail.com>
2025-08-04 15:03:48 -0300

    Insert input data SCAN

--- go-basics/investment_calculator.go
+++ go-basics/investment_calculator.go
@@ -1,11 +1,21 @@
 package main
 
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	var investmentAmount = 1000
-	var expectedReturnRate = 5.5
-	var years = 10
+	const inflationRate = 2.5
+	var investmentAmount float64
+	years := 10.0
+	expectedReturnRate := 5.5
 
-	var futereValeu = float64(investmentAmount) * math.Pow(1 + expectedReturnRate/100, float64(years))
+	fmt.Scan(&investmentAmount)
+
+	futereValeu := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futereRealValue := futereValeu / math.Pow(1+inflationRate/100, years)
+
+	fmt.Println(futereValeu)
+	fmt.Println(futereRealValue)
 }
