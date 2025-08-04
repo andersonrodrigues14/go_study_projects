0585a152f69fdf0f732aae0179b39ce80868d164
Anderson Almeida Rodrigues <anderson.a.rodrigues14@hotmail.com>
2025-08-04 15:31:42 -0300

    Add more scan variables

--- go-basics/investment_calculator.go
+++ go-basics/investment_calculator.go
@@ -8,10 +8,17 @@ import (
 func main() {
 	const inflationRate = 2.5
 	var investmentAmount float64
-	years := 10.0
-	expectedReturnRate := 5.5
+	var years float64
+	var expectedReturnRate float64
 
+	fmt.Print("Investment Amount: ")
 	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Expected Return Rate: ")
+	fmt.Scan(&expectedReturnRate)
+
+	fmt.Print("Investment Years: ")
+	fmt.Scan(&years)
 
 	futereValeu := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 	futereRealValue := futereValeu / math.Pow(1+inflationRate/100, years)
