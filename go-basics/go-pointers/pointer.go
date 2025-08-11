1c79e794c0d4eea6c84ed5cbfeb4d48a52952b08
Anderson Almeida Rodrigues <anderson.a.rodrigues14@hotmail.com>
2025-08-11 14:03:44 -0300

    Understanding Pointers

--- go-basics/go-pointers/pointer.go
+++ go-basics/go-pointers/pointer.go
@@ -11,10 +11,11 @@ func main() {
 
 	fmt.Println("Age: ", *agePointer)
 
-	adultYears := getAdultYears(agePointer)
-	fmt.Println(adultYears)
+	editAgeToAdultYeares(agePointer)
+	fmt.Println(age)
 }
 
-func getAdultYears(age *int) int {
-	return *age - 18
+func editAgeToAdultYeares(age *int) {
+	//return *age - 18
+	*age = *age - 18
 }
