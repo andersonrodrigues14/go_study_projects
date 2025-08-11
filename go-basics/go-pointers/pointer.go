34bcb654b3aab86f1f39e5349473b8a26b8b0998
Anderson Almeida Rodrigues <anderson.a.rodrigues14@hotmail.com>
2025-08-11 13:24:59 -0300

    Working with pointers

--- go-basics/go-pointers/pointer.go
+++ go-basics/go-pointers/pointer.go
@@ -5,11 +5,16 @@ import "fmt"
 func main() {
 	age := 32 // regular variable
 
-	fmt.Println("Age: ", age)
-	adultYears := getAdultYears(age)
+	var agePointer *int
+
+	agePointer = &age
+
+	fmt.Println("Age: ", *agePointer)
+
+	adultYears := getAdultYears(agePointer)
 	fmt.Println(adultYears)
 }
 
-func getAdultYears(age int) int {
-	return age - 18
+func getAdultYears(age *int) int {
+	return *age - 18
 }
