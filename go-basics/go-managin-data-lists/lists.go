99cfbc7fde9d1ac768e10fe6ddc8d088ae5d714b
Anderson Almeida Rodrigues <anderson.a.rodrigues14@hotmail.com>
2025-08-14 15:23:53 -0300

    new Module on lists started

--- go-basics/go-managin-data-lists/lists.go
+++ go-basics/go-managin-data-lists/lists.go
@@ -1,9 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	prices := [4]float64{10.96, 9.99, 45.99, 20.0}
+	prices := []float64{10.99, 8.99}
+
+	fmt.Println(prices[1])
+	prices[1] = 9.9
+
+	prices = append(prices, 5.99)
 
 	fmt.Println(prices)
+
 }
+
+// func main() {
+// 	var productNames [4]string = [4]string{"A book"}
+// 	productNames[2] = "A Carpet"
+
+// 	prices := [4]float64{10.96, 9.99, 45.99, 20.0}
+
+// 	fmt.Println(prices)
+// 	fmt.Println(productNames)
+
+// 	featurePrices := prices[1:3] // slices
+// 	fmt.Println(featurePrices)
+// 	fmt.Println(len(featurePrices)) // lenth of array
+// 	fmt.Println(cap(featurePrices)) // capacity of array
+// }
