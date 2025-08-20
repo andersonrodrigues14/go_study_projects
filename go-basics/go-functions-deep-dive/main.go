ecee2d76d0ea688c9a487a49be1477f991793d17
Anderson Almeida Rodrigues <anderson.a.rodrigues14@hotmail.com>
2025-08-19 22:25:56 -0300

    Returning Functions As Values

--- go-basics/go-functions-deep-dive/main.go
+++ go-basics/go-functions-deep-dive/main.go
@@ -12,6 +12,15 @@ func main() {
 	fmt.Println(doubled)
 	fmt.Println(tripled)
 
+	moreNumbers := []int{5,1,2}
+	transformerFn1 := getTransformerFunction(&numbers)
+	transformerFn2 := getTransformerFunction(&moreNumbers)
+
+	transformedNumbers := transformNumbers(&numbers, transformerFn1)
+	moreTransformedNumbers := transformNumbers(&moreNumbers, transformerFn2)
+	fmt.Println(transformedNumbers)
+	fmt.Println(moreTransformedNumbers)
+
 }
 
 func transformNumbers(numbers *[]int, transform transformFn) []int {
@@ -20,6 +29,14 @@ func transformNumbers(numbers *[]int, transform transformFn) []int {
 		dNumbers = append(dNumbers, transform(val))
 	}
 	return dNumbers
+}
+
+func getTransformerFunction(numbers *[]int) transformFn{
+	if (*numbers)[0] == 1 {
+		return double
+	} else {
+		return triple
+	}
 }
 
 func double(number int) int {
