2209213f333c9f84b51d974181401d06d0be4706
Anderson Almeida Rodrigues <anderson.a.rodrigues14@hotmail.com>
2025-08-20 12:36:10 -0300

    Pratice Project: Price Calculator

---
+++ go-basics/go-pratice-project-calculator/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+
+	"example.com/calculator/filemanager"
+	"example.com/calculator/prices"
+)
+
+func main() {
+	taxRates := []float64{0, 0.07, 0.1, 0.15}
+
+	for _, taxRate := range taxRates {
+		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
+		priceJob := prices.NewTaxIncludedPriceJob(fm, taxRate)
+		priceJob.Process()
+	}
+
+}
